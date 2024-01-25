ed8e108a9c0674279a17132c7e5f47bd2921934a
LuisWilke <wilke_lopes@hotmail.com>
2024-01-25 09:51:32 -0400

    :tada:

---
+++ Menu.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue; // Cor de fundo do terminal
+            Console.ForegroundColor = ConsoleColor.Black; // Cor da letra do terminal
+
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i < 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i < 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i < 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EditorHTML;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
